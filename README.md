15efe6537af823c96aebb328d94e88989d593243
Daniel Buchner <danieljb2@gmail.com>
2013-09-09 15:10:27 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
 # About
 
-This repo is an X-Tag custom web-component stub that can be used as a starting point when building new elements.  It provides that basic tools and grunt tasks to get started.
-
-# Dev Setup
-
-```
-Fork this repo, rename it, then clone it.
-
-$ npm install	// install bower tasks
-$ bower install	// install components
-$ grunt build   // build the dependencies
-
-```
-
-# Create X-Tag Components
-
-[Guide for creating X-Tag Components](https://github.com/x-tag/core/wiki/Creating-X-Tag-Components)
-
-# Use X-Tag Components
-
-[Using X-Tag components in your applications](https://github.com/x-tag/core/wiki/Using-X-Tag-Components-in-your-application)
-
-
+Adds the ability to detect when events occur outside of the listening element
--- src/main.js
+++ src/main.js
@@ -1,5 +1,49 @@
 (function(){  
 
-// Create your component here
+  function outerNodes(element, pseudo){
+    var src = pseudo.source,
+        outer = xtag.pseudos.outer, 
+        nodes = outer.__nodes__[src.type] || (outer.__nodes__[src.type] = []),
+        pseudos = outer.__pseudos__[src.type] || (outer.__pseudos__[src.type] = []),
+        index = nodes.indexOf(element);
+    if (index == -1) {
+      nodes.push(element);
+      pseudos.push(pseudo);
+    }
+    else {
+      nodes.splice(index, 1);
+      pseudos.splice(index, 1);
+    }
+    return nodes;
+  }
+  
+  xtag.pseudos.outer = {
+    __nodes__: {},
+    __pseudos__: {},
+    __observers__: {},
+    action: function(pseudo, e){
+      if (this == e.target || this.contains && this.contains(e.target)) return null;
+    },
+    onRemove: function(pseudo){
+      if (!outerNodes(this, pseudo).length) {
+        xtag.removeEvent(document, xtag.pseudos.outer.__observers__[pseudo.source.type]);
+      }
+    },
+    onAdd: function(pseudo){
+      outerNodes(this, pseudo);
+      var element = this,
+          type = pseudo.source.type,
+          outer = xtag.pseudos.outer;
+      if (!outer.__observers__[type]) {
+        outer.__observers__[type] = xtag.addEvent(document, type, function(e){
+          outer.__nodes__[type].forEach(function(node, i){
+            if (node == e.target || node.contains(e.target)) return;
+            var pseudo = outer.__pseudos__[type][i];
+            pseudo.source.stack.call(node, e);
+          });
+        });
+      }
+    }
+  }
 
 })();
